d63c181f30d3d390a0e68532112283e88cfd4738
Roman Sokolov <still2.mail@me.com>
2016-10-06 20:28:05 +0300

    https://github.com/AdguardTeam/AdguardForiOS/issues/170

---
+++ AdguardExtension/AdguardPro/UI/APUIDnsStatusTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  APUIDnsStatusTableViewCell.h
+//  Adguard
+//
+//  Created by Roman Sokolov on 06.10.16.
+//  Copyright © 2016 Performiks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APUIDnsStatusTableViewCell : UITableViewCell
+
+@end
---
+++ AdguardExtension/AdguardPro/UI/APUIDnsStatusTableViewCell.m
@@ -0,0 +1,74 @@
+//
+//  APUIDnsStatusTableViewCell.m
+//  Adguard
+//
+//  Created by Roman Sokolov on 06.10.16.
+//  Copyright © 2016 Performiks. All rights reserved.
+//
+
+#import "APUIDnsStatusTableViewCell.h"
+
+@implementation APUIDnsStatusTableViewCell {
+    
+    UISwitch *_statusSwitch;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+    
+    _statusSwitch = self.accessoryView.subviews[0];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (BOOL)isAccessibilityElement {
+    
+    return YES;
+}
+
+- (NSString *)accessibilityValue {
+    
+    return [NSString string];
+}
+
+- (NSString *)accessibilityLabel {
+    
+    NSString *label = [super accessibilityLabel];
+    
+    return label;
+}
+- (UIAccessibilityTraits)accessibilityTraits {
+    
+    UIAccessibilityTraits traits = [super accessibilityTraits];
+    traits &= ~UIAccessibilityTraitSelected;
+//    traits |= UIAccessibilityTraitButton;
+    
+    traits |= _statusSwitch.accessibilityTraits;
+    return traits;
+}
+
+//- (NSString *)accessibilityHint {
+//    
+//    NSString *hint = _statusSwitch.accessibilityHint;
+//    NSLog(@"Hint: %@", hint);
+//    return hint;
+//}
+
+- (BOOL)accessibilityActivate {
+    
+    if (_statusSwitch.enabled) {
+        _statusSwitch.on = ! _statusSwitch.on;
+        [_statusSwitch sendActionsForControlEvents:UIControlEventValueChanged];
+//        return YES;
+    }
+    
+    return NO;
+}
+
+
+@end
