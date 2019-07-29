541c224f2baf9370067bd9429291d803b1eb0630
Ivan Ilin <ivanilin@192-168-11-219.office.flops.ru>
2019-07-29 15:01:51 +0300

    add login button on about screen

--- AdguardExtension/AdguardApp/UI/ViewControllers/About/AboutController.swift
+++ AdguardExtension/AdguardApp/UI/ViewControllers/About/AboutController.swift
@@ -17,8 +17,9 @@
  */
 
 import Foundation
+import SafariServices
 
-class AboutController : UIViewController {
+class AboutController : UIViewController, UIViewControllerTransitioningDelegate, LoginControllerDelegate {
     
     let otherPlatformsAction = "http://agrd.io/ios_adguard_products"
     
@@ -26,9 +27,14 @@ class AboutController : UIViewController {
     
     @IBOutlet var themableLabels: [ThemableLabel]!
     @IBOutlet weak var logoImage: ThemeableImageView!
+    @IBOutlet var loginButton: UIBarButtonItem!
     
-     lazy var theme: ThemeServiceProtocol = { ServiceLocator.shared.getService()! }()
+    private let theme: ThemeServiceProtocol = ServiceLocator.shared.getService()!
+    private let purchaseService: PurchaseService = ServiceLocator.shared.getService()!
     
+    private var notificationObserver: Any?
+    var canShowAlert = false
+    var showAlertBlock: (()->Void)?
     
     // UIViewController life cycle
     
@@ -36,6 +42,21 @@ class AboutController : UIViewController {
         super.viewDidLoad()
         
         versionLabel.text = ADProductInfo.versionWithBuildNumber()
+        
+        notificationObserver = NotificationCenter.default.addObserver(forName: Notification.Name(PurchaseService.kPurchaseServiceNotification),
+                                                                      object: nil, queue: nil)
+        { [weak self](notification) in
+            
+            DispatchQueue.main.async {
+                if let info = notification.userInfo {
+                    self?.processNotification(info: info)
+                    self?.updateViews()
+                    self?.updateTheme()
+                }
+            }
+        }
+        
+        updateViews()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -52,6 +73,22 @@ class AboutController : UIViewController {
         }
     }
     
+    @IBAction func login(_ sender: Any) {
+        showLoginDialog()
+    }
+    
+    // MARK: - Presentation delegate methods
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return CustomAnimatedTransitioning()
+    }
+    
+    // MARK: - LoginControllerDelegate methods
+    
+    func loginAction(name: String) {
+        webAuthWithName(name: name)
+    }
+    
     // MARK: - private methods
     
     private func updateTheme() {
@@ -59,5 +96,100 @@ class AboutController : UIViewController {
         theme.setupNavigationBar(navigationController?.navigationBar)
         theme.setupLabels(themableLabels)
         theme.setupImage(logoImage)
+    }
+    
+    private func showLoginDialog() {
+        let storyboard = UIStoryboard(name: "GetPro", bundle: Bundle.main)
+        guard let controller = storyboard.instantiateViewController(withIdentifier: "LoginController") as? LoginController else { return }
+        controller.modalPresentationStyle = .custom
+        controller.transitioningDelegate = self
+        controller.delegate = self
+        
+        present(controller, animated: true, completion: nil)
+    }
+    
+    private func webAuthWithName(name: String){
+        
+        DDLogInfo("(GetProController) - webAuth")
+        guard let url = purchaseService.authUrlWithName(name: name) else { return }
+        let safariController = SFSafariViewController(url: url)
+        present(safariController, animated: true, completion: nil)
+        canShowAlert = false
+    }
+    
+    private func processNotification(info: [AnyHashable: Any]) {
+        
+        DispatchQueue.main.async { [weak self] in
+            let type = info[PurchaseService.kPSNotificationTypeKey] as? String
+            let error = info[PurchaseService.kPSNotificationErrorKey] as? NSError
+            
+            switch type {
+            case PurchaseService.kPSNotificationLoginSuccess:
+                self?.loginSuccess()
+            case PurchaseService.kPSNotificationLoginFailure:
+                self?.loginFailure(error: error)
+            case PurchaseService.kPSNotificationLoginPremiumExpired:
+                self?.premiumExpired()
+            case PurchaseService.kPSNotificationLoginNotPremiumAccount:
+                self?.notPremium()
+                
+            case PurchaseService.kPSNotificationOauthSucceeded:
+                self?.authSucceeded()
+                
+            default:
+                break
+            }
+        }
+    }
+    
+    private func loginSuccess(){
+        loginCompleteWithMessage(message: ACLocalizedString("login_success_message", nil))
+    }
+    
+    private func loginFailure(error: NSError?) {
+        loginCompleteWithMessage(message: ACLocalizedString("login_error_message", nil))
+    }
+    
+    private func premiumExpired() {
+        loginCompleteWithMessage(message: ACLocalizedString("login_premium_expired_message", nil))
+    }
+    
+    private func notPremium() {
+        loginCompleteWithMessage(message: ACLocalizedString("not_premium_message", nil))
+    }
+    
+    private func loginCompleteWithMessage(message: String) {
+        
+        showAlertBlock = { [weak self] in
+            guard let sSelf = self else { return }
+            sSelf.removeLoading() {
+                ACSSystemUtils.showSimpleAlert(for: sSelf, withTitle: nil, message: message)
+            }
+        }
+        
+        showAlertIfPossible()
+    }
+    
+    func authSucceeded() {
+        if self.presentedViewController != nil {
+            
+            self.presentedViewController?.dismiss(animated: true) { [weak self] in
+                guard let sSelf = self else { return }
+                sSelf.canShowAlert = true
+                sSelf.showAlertIfPossible()
+            }
+        }
+    }
+    
+    private func showAlertIfPossible() {
+        if canShowAlert && showAlertBlock != nil {
+            showAlertBlock!()
+            showAlertBlock = nil
+        }
+    }
+    
+    private func updateViews() {
+        let loggedIn = purchaseService.isProPurchased && purchaseService.purchasedThroughLogin
+        self.navigationItem.rightBarButtonItem = loggedIn ? nil : loginButton
     }
 }
