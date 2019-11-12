1fff122ed410de0f27fca1cae1750cc67756a1aa
Ilya Kovalchuk <i.kovalchuk@adguard.com>
2019-11-12 11:08:02 +0300

    List of rules screen fixed

--- AdguardExtension/AdguardApp/UI/ViewControllers/ListOfRules/Container's controllers/ListOfRulesTableController.swift
+++ AdguardExtension/AdguardApp/UI/ViewControllers/ListOfRules/Container's controllers/ListOfRulesTableController.swift
@@ -104,6 +104,14 @@ class ListOfRulesTableController: UITableViewController, ListOfRulesModelDelegat
     override func numberOfSections(in tableView: UITableView) -> Int {
         return state == .normal ? 3 : 1
     }
+    
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return section == enableListOfRulesSection ? 20.0 : super.tableView(tableView, heightForFooterInSection: section)
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if state == .normal {
