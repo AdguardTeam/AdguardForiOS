4e905e88256f466f7decaede39c5c11b2d8a9ede
Ivan Ilin <ivanilin@192-168-11-219.office.flops.ru>
2019-12-13 17:33:11 +0300

    nothing

--- AdguardExtension/Tunnel/DnsLogRecordsWriter.swift
+++ AdguardExtension/Tunnel/DnsLogRecordsWriter.swift
@@ -16,7 +16,7 @@
        along with Adguard for iOS.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import Foundation
+import Foundation 
 
 class DnsLogRecordsWriter: NSObject, DnsLogRecordsWriterProtocol {
     
