4f4fd727e2e16b09c28f25d9f860132f090c6790
Sergey Berdov <s.berdov@adguard.com>
2021-03-10 12:32:48 +0300

    Changed digits fraction for count labels, added seconds for average time label

--- Libs/ACommons/Lang/String+Utils.swift
+++ Libs/ACommons/Lang/String+Utils.swift
@@ -169,10 +169,17 @@ extension String {
         formatter.locale = .current
         formatter.minimumFractionDigits = 0
         
-        let seconds = number.doubleValue / 1000
+        let seconds = number.doubleValue / 1000000
         if seconds >= 1 {
-            formatter.maximumFractionDigits = 1
+            formatter.maximumFractionDigits = 0
             let formatterString = formatter.string(from: NSNumber(floatLiteral: seconds)) ?? "\(number.intValue)"
+            return String(format: String.localizedString("s_unit"), formatterString)
+        }
+        
+        let miliseconds = number.doubleValue / 1000
+        if miliseconds >= 1 {
+            formatter.maximumFractionDigits = 1
+            let formatterString = formatter.string(from: NSNumber(floatLiteral: miliseconds)) ?? "\(number.intValue)"
             return String(format: String.localizedString("ms_unit"), formatterString)
         }
         
@@ -202,7 +209,7 @@ extension String {
         formatter.numberStyle = .decimal
         formatter.locale = .current
         formatter.minimumFractionDigits = 0
-        formatter.maximumFractionDigits = 2
+        formatter.maximumFractionDigits = 0
         
         let decimalNumber: Double = number.doubleValue
         
