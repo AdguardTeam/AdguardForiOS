1d8d22d494fc3d7e8aa66c70deccd1d2c1674304
Sergey Berdov <s.berdov@adguard.com>
2021-03-26 18:16:43 +0300

    Removed login/logout button from About screen

--- AdguardExtension/AdguardApp/UI/ViewControllers/MainTabBar/MainMenu/About/AboutViewController.swift
+++ AdguardExtension/AdguardApp/UI/ViewControllers/MainTabBar/MainMenu/About/AboutViewController.swift
@@ -26,17 +26,12 @@ class AboutViewController: UIViewController {
     @IBOutlet weak var copyrightLabel: ThemableLabel!
     @IBOutlet var themableLabels: [ThemableLabel]!
     
-    @IBOutlet weak var loginLogoutButton: UIButton!
     
     private let theme: ThemeServiceProtocol = ServiceLocator.shared.getService()!
     private let configuration: ConfigurationService = ServiceLocator.shared.getService()!
-    private let purchaseService: PurchaseServiceProtocol = ServiceLocator.shared.getService()!
     private let productInfo: ADProductInfoProtocol = ServiceLocator.shared.getService()!
     
     private var themeToken: NotificationToken?
-    private var proStatusObservation: NSKeyValueObservation?
-    
-    private let loginSegueId = "loginSegueId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,13 +40,6 @@ class AboutViewController: UIViewController {
             self?.updateTheme()
         }
         
-        proStatusObservation = configuration.observe(\.proStatus) {(_, _) in
-            DispatchQueue.main.async {[weak self] in
-                self?.processProStatus()
-            }
-        }
-        
-        processProStatus()
         setupLabels()
         updateTheme()
         setupBackButton()
@@ -75,38 +63,12 @@ class AboutViewController: UIViewController {
         UIApplication.shared.openAdguardUrl(action: "privacy", from: "about", buildVersion: productInfo.buildVersion())
     }
     
-    @IBAction func loginLogoutTapped(_ sender: UIButton) {
-        if purchaseService.purchasedThroughLogin {
-            showLogoutAlert()
-        } else {
-            performSegue(withIdentifier: loginSegueId, sender: self)
-        }
-    }
-    
     // MARK: - private methods
     
     private func updateTheme() {
         view.backgroundColor = theme.backgroundColor
         theme.setupLabels(themableLabels)
         theme.setupImage(logoImageView)
-    }
-    
-    private func processProStatus() {
-        navigationItem.rightBarButtonItems = configuration.proStatus ? [] : [loginButton]
-        
-        if Bundle.main.isPro || configuration.proStatus && !purchaseService.purchasedThroughLogin {
-            loginLogoutButton.isHidden = true
-            return
-        }
-        
-        let redColor = UIColor(hexString: "#DF3812")
-        let greenColor = UIColor.AdGuardColor.green
-        
-        let color = configuration.proStatus ? redColor : greenColor
-        let title = configuration.proStatus ? String.localizedString("common_logout") : String.localizedString("common_license")
-        
-        loginLogoutButton.setTitleColor(color, for: .normal)
-        loginLogoutButton.setTitle(title, for: .normal)
     }
     
     private func setupLabels() {
@@ -119,29 +81,5 @@ class AboutViewController: UIViewController {
         let currentYearString = dateFormatter.string(from: Date())
         let copyrightFormat = String.localizedString("copyright_format")
         copyrightLabel.text = String(format: copyrightFormat, currentYearString)
-    }
-    
-    private func showLogoutAlert() {
-        let alert = UIAlertController(title: nil, message: ACLocalizedString("confirm_logout_text", nil), preferredStyle: .actionSheet)
-        
-        let cancelAction = UIAlertAction(title: ACLocalizedString("common_action_cancel", nil), style: .cancel, handler: nil)
-        alert.addAction(cancelAction)
-        
-        let okAction = UIAlertAction(title: ACLocalizedString("common_action_yes", nil), style: .destructive) {
-            [weak self] (action) in
-            if self?.purchaseService.logout() ?? false {
-                DispatchQueue.main.async {
-                    self?.processProStatus()
-                }
-            }
-        }
-        alert.addAction(okAction)
-        
-        if let presenter = alert.popoverPresentationController{
-            presenter.sourceView = loginLogoutButton
-            presenter.sourceRect = loginLogoutButton.bounds
-        }
-        
-        self.present(alert, animated: true, completion: nil)
     }
 }
