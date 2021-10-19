3c61c43da6a4b6bbfee061652b865f26a7dc8553
Ivan Ilin <i.ilin@adguard.com>
2021-10-19 13:16:32 +0300

    dns server name fix

---
+++ AdguardExtension/AdguardApp/Services/DnsProvidersManagerProtocol+ServerName.swift
@@ -0,0 +1,54 @@
+
+/**
+    This file is part of Adguard for iOS (https://github.com/AdguardTeam/AdguardForiOS).
+    Copyright © Adguard Software Limited. All rights reserved.
+
+    Adguard for iOS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Adguard for iOS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Adguard for iOS.  If not, see <http://www.gnu.org/licenses>.
+*/
+  
+
+import DnsAdGuardSDK
+
+extension DnsProvidersManagerProtocol {
+    var activeServerName: String {
+        let name: String
+        let provider = self.activeDnsProvider
+        if provider.isCustom {
+            name = provider.activeServerName
+        }
+        else {
+            name = "\(provider.activeServerName) (\(self.activeDnsServer.type.name))"
+        }
+        return name
+    }
+}
+
+extension DnsProtocol {
+    var name: String {
+        let key: String
+        switch self {
+        case .dns:
+            key = "regular_dns_protocol"
+        case .dnscrypt:
+            key = "dns_crypt_protocol"
+        case .doh:
+            key = "doh_protocol"
+        case .dot:
+            key = "dot_protocol"
+        case .doq:
+            key = "doq_protocol"
+        }
+        return String.localizedString(key)
+    }
+}
