b203ed741dc6af617d6e834964f47786cca84114
Artem Ivanov <a.ivanov@adguard.com>
2021-11-01 03:34:35 +0300

    💉 Enhance: Replace header for a lot of files

--- Libs/ASConstants.h
+++ Libs/ASConstants.h
@@ -9,11 +9,11 @@
 ///
 /// Adguard for iOS is distributed in the hope that it will be useful,
 /// but WITHOUT ANY WARRANTY; without even the implied warranty of
-/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 /// GNU General Public License for more details.
 ///
 /// You should have received a copy of the GNU General Public License
-/// along with Adguard for iOS.  If not, see <http://www.gnu.org/licenses/>.
+/// along with Adguard for iOS. If not, see <http://www.gnu.org/licenses/>.
 ///
 
 #ifndef ASConstants_h
