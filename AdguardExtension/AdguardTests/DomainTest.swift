a8e563ecfbd609a6e15283d60f8cdf652afc745e
Sergey Berdov <s.berdov@adguard.com>
2021-12-08 15:45:34 +0300

    Tests

--- AdguardExtension/AdguardTests/DomainTest.swift
+++ AdguardExtension/AdguardTests/DomainTest.swift
@@ -20,27 +20,25 @@ import XCTest
 
 class DomainTest: XCTestCase {
 
-    let domains = [ "some.string.with.domain.and.subdomains.com" : "some.string.with.domain.and.subdomains.com",
-                    "foo.bar" : "foo.bar",
-                    "some-string.with123.dom--ain.and.subdomains.com" : "some-string.with123.dom--ain.and.subdomains.com",
-                    "하위.도메인이.있는.일부.줄.한국" : "하위.도메인이.있는.일부.줄.한국",
-                    "фуу.бар" : "фуу.бар",
-                    "하-위.도메123인--이.있는.일부.줄.한국" : "하-위.도메123인--이.있는.일부.줄.한국",
-                    "someDomain" : nil,
-                    "someDomain#$%$" : nil,
-                    "someDomain..net" : nil,
-                    "foo.b" : nil,
-                    "some-.domain" : nil ]
+    let domains = [ "some.string.with.domain.and.subdomains.com" : ["some.string.with.domain.and.subdomains.com"],
+                    "foo.bar" : ["foo.bar"],
+                    "some-string.with123.dom--ain.and.subdomains.com" : ["some-string.with123.dom--ain.and.subdomains.com"],
+                    "하위.도메인이.있는.일부.줄.한국" : ["하위.도메인이.있는.일부.줄.한국"],
+                    "фуу.бар" : ["фуу.бар"],
+                    "하-위.도메123인--이.있는.일부.줄.한국" : ["하-위.도메123인--이.있는.일부.줄.한국"],
+                    "someSchema://somedomain.with.sub.domains.com?parameter=another.domain.with.sub.domains.ru" : ["somedomain.with.sub.domains.com",
+                                                                                                                   "another.domain.with.sub.domains.ru"],
+                    "someDomain" : [],
+                    "someDomain#$%$" : [],
+                    "someDomain..net" : [],
+                    "foo.b" : [],
+                    "some-.domain" : [],
+                    "" : [],
+                    " " : [] ]
 
     func testFindDomains() {
         domains.forEach {
-            XCTAssertEqual(Domain.findDomains(in: $0).first, $1)
+            XCTAssertEqual(Domain.findDomains(in: $0), $1)
         }
-
-        let multipleDomainsWithSubdomains = "someSchema://somedomain.with.sub.domains.com?parameter=another.domain.with.sub.domains.ru"
-        let result = Domain.findDomains(in: multipleDomainsWithSubdomains)
-        XCTAssertEqual(result.count, 2)
-        XCTAssertEqual(result[0], "somedomain.with.sub.domains.com")
-        XCTAssertEqual(result[1], "another.domain.with.sub.domains.ru")
     }
 }
