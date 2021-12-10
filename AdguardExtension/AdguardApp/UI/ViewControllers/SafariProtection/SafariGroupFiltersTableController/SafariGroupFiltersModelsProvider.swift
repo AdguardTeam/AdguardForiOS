e7b625314b4dd79c1839df808ed1b2fc72167c94
Sergey Berdov <s.berdov@adguard.com>
2021-12-10 13:46:08 +0300

    Sorting tags with lang

--- AdguardExtension/AdguardApp/UI/ViewControllers/SafariProtection/SafariGroupFiltersTableController/SafariGroupFiltersModelsProvider.swift
+++ AdguardExtension/AdguardApp/UI/ViewControllers/SafariProtection/SafariGroupFiltersTableController/SafariGroupFiltersModelsProvider.swift
@@ -62,7 +62,7 @@ final class SafariGroupFiltersModelsProvider {
             self.initialGroupModels.append(groupModel)
 
             let filtersModels = group.filters.map { filter -> SafariFilterCellModel in
-                let tagsModels = filter.tags.map { SafariTagButtonModel(tag: $0, isSelected: true) }
+                let tagsModels = getSafariTagButtonModelsSortedByLangTagType(tags: filter.tags)
                 return SafariFilterCellModel(
                     filterId: filter.filterId,
                     groupType: group.groupType,
@@ -138,5 +138,14 @@ final class SafariGroupFiltersModelsProvider {
         } else {
             return nil
         }
+    }
+
+    /// Returns sorted `SafariTagButtonModels` by lang tag type
+    private func getSafariTagButtonModelsSortedByLangTagType(tags: [ExtendedFiltersMeta.Tag]) -> [SafariTagButtonModel] {
+         return tags.sorted { lhs, rhs in
+             let lhsWeight = lhs.tagType == .lang ? 0 : 1
+             let rhsWeight = rhs.tagType == .lang ? 0 : 1
+            return lhsWeight < rhsWeight
+        }.map { SafariTagButtonModel(tag: $0, isSelected: true) }
     }
 }
