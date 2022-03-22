80ed8aa566acbf6060ca08236aff88ae0240dd35
Aleksey Dmitrevskiy <ad@adguard.com>
2022-03-22 22:11:12 +0300

    💉 Enhance: Add legacy app detection

--- AdguardExtension/AdguardApp/UI/Utils/UIApplicationExtensions.swift
+++ AdguardExtension/AdguardApp/UI/Utils/UIApplicationExtensions.swift
@@ -1,9 +1,32 @@
 import Foundation
 
 extension UIApplication {
-    static var isOldAdGuardInstalled: Bool {
-        let adGuard = URL(string: "")!
-        let adGuardPro = URL(string: "")!
-        return shared.canOpenURL(adGuardPro) || shared.canOpenURL(adGuardPro)
+
+    /// Checks the type of a legacy app installed on the device
+    func detectLegacyAppInstalled() -> LegacyAppType? {
+        if canOpenUrl(scheme: "transfer-scheme-adguard-pro:") {
+            return .adguardPro
+        } else if canOpenUrl(scheme: "transfer-scheme-adguard:") {
+            return .adguard
+        }
+        return nil
+    }
+
+    private func canOpenUrl(scheme: String) -> Bool {
+        guard let url = URL(string: scheme) else {
+            DDLogInfo("Failed to create an url for scheme \(scheme)")
+            return false
+        }
+
+        return UIApplication.shared.canOpenURL(url)
+    }
+
+    /// An enum representation of a legacy app installed
+    enum LegacyAppType {
+        // Regular AdGuard
+        case adguard
+
+        // AdGuardPro
+        case adguardPro
     }
 }
