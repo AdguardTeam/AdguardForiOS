e9c9cc2d1989e7f173b44e1159b2848ebaa42521
Aleksey Dmitrevskiy <ad@adguard.com>
2022-04-19 17:41:50 +0300

    🛠 Chore: Enable dev backend for remote migration

--- Libs/Networking/Requests/RemoteMigrationRequest.swift
+++ Libs/Networking/Requests/RemoteMigrationRequest.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Object thant generate URL request for remote migration
 final class RemoteMigrationRequest : RequestProtocol {
-    private static let BACKEND_DOMAIN = "https://mobile.adtidy.org"
+    private static let BACKEND_DOMAIN = "https://testmobile.adtidy.org"
     private static let MIGRATION_URL = "/api/1.0/ios_migration/\(LoginService.APP_TYPE_VALUE)/status"
 
     private let appId: String
