0dec2ed62cf28dc66abc87158e88a9f1426c3ffb
Slava Leleka <v.leleka@adguard.com>
2023-03-10 18:49:12 +0200

    make Engine sync

--- AdguardExtension/SafariWebExtension/extension/src/pages/background/engine/Engine.ts
+++ AdguardExtension/SafariWebExtension/extension/src/pages/background/engine/Engine.ts
@@ -3,12 +3,14 @@ import { CosmeticOption, RequestType } from '@adguard/tsurlfilter';
 
 import { app } from '../app';
 
-export class Engine {
+/**
+ * Sync engine.
+ */
+export class EngineSync {
     engine: TSUrlFilter.Engine | undefined;
 
-    async start(rulesText: string): Promise<TSUrlFilter.Engine> {
+    start(rulesText: string): TSUrlFilter.Engine {
         const FILTER_ID = 1;
-        const ASYNC_LOAD_CHUNK_SIZE = 5000;
 
         const rulesList = new TSUrlFilter.StringRuleList(
             FILTER_ID,
@@ -31,48 +33,44 @@ export class Engine {
 
         this.engine = new TSUrlFilter.Engine(ruleStorage, true);
 
-        /*
-         * UI thread becomes blocked on the options page while request filter is created
-         * that's why we create filter rules using chunks of the specified length
-         * Request filter creation is rather slow operation so we should
-         * use setTimeout calls to give UI thread some time.
-        */
-        await this.engine.loadRulesAsync(ASYNC_LOAD_CHUNK_SIZE);
+        // synchronously load the rules
+        this.engine.loadRules();
 
         return this.engine;
     }
+}
 
-    getMatchingResult = (url: string) => {
-        if (!this.engine) {
-            return new TSUrlFilter.MatchingResult([], null);
-        }
-
-        const request = new TSUrlFilter.Request(
-            url,
-            url,
-            RequestType.Document,
-        );
-
-        // TODO should here to be generated allowlist rule if necessary?
-        const frameRule = null;
-
-        return this.engine.matchRequest(request, frameRule);
-    };
-
-    getCosmeticOption(url: string) {
-        const matchingResult = this.getMatchingResult(url);
-        return matchingResult.getCosmeticOption();
+const getMatchingResult = (url: string, engine: TSUrlFilter.Engine | undefined) => {
+    if (!engine) {
+        return new TSUrlFilter.MatchingResult([], null);
     }
 
-    /**
-     * Gets cosmetic result for the specified hostname and cosmetic options
-     */
-    getCosmeticResult = (hostname: string, cosmeticOption: CosmeticOption) => {
-        if (!this.engine) {
-            return new TSUrlFilter.CosmeticResult();
-        }
+    const request = new TSUrlFilter.Request(
+        url,
+        url,
+        RequestType.Document,
+    );
 
-        const request = new TSUrlFilter.Request(hostname, null, TSUrlFilter.RequestType.Document);
-        return this.engine.getCosmeticResult(request, cosmeticOption);
-    };
+    // TODO: should here to be generated allowlist rule if necessary?
+    const frameRule = null;
+
+    return engine.matchRequest(request, frameRule);
+};
+
+export const getCosmeticOption = (url: string, engine: TSUrlFilter.Engine | undefined) => {
+    const matchingResult = getMatchingResult(url, engine);
+    return matchingResult.getCosmeticOption();
 }
+
+export const getCosmeticResult = (
+    hostname: string,
+    cosmeticOption: CosmeticOption,
+    engine: TSUrlFilter.Engine | undefined,
+) => {
+    if (!engine) {
+        return new TSUrlFilter.CosmeticResult();
+    }
+
+    const request = new TSUrlFilter.Request(hostname, null, TSUrlFilter.RequestType.Document);
+    return engine.getCosmeticResult(request, cosmeticOption);
+};
