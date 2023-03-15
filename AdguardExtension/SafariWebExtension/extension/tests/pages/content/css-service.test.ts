8e94fdc58726446aa0bf30c527f96332ea9d31cf
Slava Leleka <v.leleka@adguard.com>
2023-03-15 16:17:51 +0200

    update tests for css-service

--- AdguardExtension/SafariWebExtension/extension/tests/pages/content/css-service.test.ts
+++ AdguardExtension/SafariWebExtension/extension/tests/pages/content/css-service.test.ts
@@ -1,5 +1,5 @@
 import { CosmeticRule } from '@adguard/tsurlfilter';
-import { buildStyleSheet } from '../../../src/pages/background/css-service';
+import { buildStyleSheet } from '../../../src/pages/content/css-service';
 
 describe('css-service', () => {
     it('returns one css rule in one array element for inject rules', () => {
